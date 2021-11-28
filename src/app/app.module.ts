ec2686c7c61f1261d27474379c2107e43787f6d0
Konny Amaya <konnylizamaya@gmail.com>
2021-11-27 21:52:20 -0600

    conexion con firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// importando librerias de firebase
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+// segun https://github.com/angular/angularfire/blob/master/docs/auth/getting-started.md
+// esta lib auth no es necesario que este aqui, si no que se usa directamente en el component
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
